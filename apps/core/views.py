b9ff94b66f315f9c62d6a25ca760186779311802
ragnarok <angelriera1796@gmail.com>
2022-09-17 10:14:17 -0400

    contact cards ready

--- apps/core/views.py
+++ apps/core/views.py
@@ -3,9 +3,31 @@ from django.shortcuts import render
 from products.models import Product
 from django.utils.translation import gettext
 # Create your views here.
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
 
 
 def index(request):
+
+    if request.method == "GET":
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data["subject"]
+            from_email = form.cleaned_data["from_email"]
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ["admin@example.com"])
+            except BadHeaderError:
+                return HttpResponse("Invalid header found.")
+            return redirect("contacto")
+
+
+
     populares = Product.objects.filter(destacado=True)
     carrusel = [
         {
@@ -23,6 +45,7 @@ def index(request):
     ingredientes = Product.objects.all()
 
     ctx = {
+        "form":form,
         "ingre": ingredientes,
         "carrusel": carrusel,
         "populares": populares
@@ -36,10 +59,7 @@ def about(request):
     ctx = {}
     return render(request, 'core/about/about.html', ctx)
 
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
-from .forms import ContactForm
+
 
 
 def contactView(request):
