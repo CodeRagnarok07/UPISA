8b6ec01de40470c0cea2c802121117ebefb27f86
ragnarok <angelriera1796@gmail.com>
2022-10-15 16:07:28 -0400

    correcciones

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import parrilleros, viewDetail
-
+from .views import parrilleros, viewDetail, novedades, novedadesViewDetail
 
 
 urlpatterns = [
     path('parrilleros', parrilleros, name='parrilleros'),
     path('parrilleros/<str:url>', viewDetail, name='detail_parrilleros'),
+    path('novedades', novedades, name='novedades'),
+    path('novedades/<str:url>', novedadesViewDetail, name='detail_novedades'),
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import TrucosYConsejos
+from .models import TrucosYConsejos , Novedades
 # Create your views here.
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
@@ -23,6 +23,29 @@ def viewDetail(request, url):
 
     post = get_object_or_404(TrucosYConsejos, url=url)
     similares = list(TrucosYConsejos.objects.all().exclude(url=url))
+    if len(similares) > 2:
+        similares = random.sample(similares, 3)
+    ctx ={
+        "post":post,
+        "more":similares
+    }
+    return render(request, 'blog/detail.html', ctx)
+
+
+def novedades(request):
+    pots = Novedades.objects.all()
+    paginator = Paginator(pots, 8) # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+    ctx = {
+        "page_obj": page_obj
+    }
+    return render(request, 'blog/novedades.html', ctx)
+
+def novedadesViewDetail(request, url):
+    post = get_object_or_404(Novedades, url=url)
+    similares = list(Novedades.objects.all().exclude(url=url))
     if len(similares) > 2:
         similares = random.sample(similares, 3)
     ctx ={
