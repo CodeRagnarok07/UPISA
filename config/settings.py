ef6eb56721efcc03adf072e454f77d94aaa05d28
ragnarok <angelriera1796@gmail.com>
2022-10-23 20:51:49 -0400

    collectstatic

--- config/settings.py
+++ config/settings.py
@@ -174,7 +174,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 
 MEDIA_URL = "/media/"
