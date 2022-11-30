62ad33ee028b57aef5f541a07e7e2f340e124c35
ragnarok <angelriera1796@gmail.com>
2022-11-29 22:46:13 -0400

    .

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
