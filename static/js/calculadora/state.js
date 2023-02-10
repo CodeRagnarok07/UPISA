fc467a558858a9f5acae9e22e720366d2d60a796
ragnarok <angelriera1796@gmail.com>
2023-02-09 21:04:08 -0400

    calculadora y refacturacion de archivos scss

---
+++ static/js/calculadora/state.js
@@ -0,0 +1,87 @@
+
+const state = {
+    personas: [
+        {
+            tipo: "Adultos",
+            cantidad: 1,
+            porcion: 500,
+        },
+        {
+            tipo: "Niños",
+            cantidad: 1,
+            porcion: 250,
+        }
+
+    ],
+    carnes: [
+        {
+            name: "Bondiola",
+            used: false,
+            gpk: 100
+        },
+        {
+            name: "tapa cuadril",
+            used: false,
+            gpk: 100
+        },
+        {
+            name: "costilla",
+            used: false,
+            gpk: 100
+        },
+        {
+            name: "asado americano",
+            used: false,
+            gpk: 100
+        },
+        {
+            name: "bife de chorizo",
+            used: false,
+            gpk: 100
+        },
+        {
+            name: "matambrito",
+            used: false,
+            gpk: 100
+        },
+
+    ],
+    embutidos: [
+        {
+            name: "toscana tradicional",
+            used: false,
+            gpk: 100
+        },
+        {
+            name: "toscana con finas hierbas",
+            used: false,
+            gpk: 100
+        },
+        {
+            name: "toscana con ajo",
+            used: false,
+            gpk: 100
+        },
+        {
+            name: "toscana picante",
+            used: false,
+            gpk: 100
+        },
+        {
+            name: "chorizo de viena",
+            used: false,
+            gpk: 100
+        },
+        {
+            name: "parrillero con y sin picante",
+            used: false,
+            gpk: 100
+        },
+
+    ]
+
+}
+
+
+
+export default state
