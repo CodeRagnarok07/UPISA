8b7b7854bd4a23209fc69f6e61eccbbe3ed21ca1
ragnarok <angelriera1796@gmail.com>
2023-03-21 17:30:14 -0400

    refact

--- config/settings.py
+++ config/settings.py
@@ -94,7 +94,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['static/templates/'],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
