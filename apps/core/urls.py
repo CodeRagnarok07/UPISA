90751d30d60c2c964be49e755aaea0415246e17b
angeldev <angelriera1796@gmail.com>
2023-07-06 19:28:06 -0400

    promo page

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('empresa/', TemplateView.as_view(template_name="empresa/index.html"), name='empresa'),
     path('cerdo/', TemplateView.as_view(template_name="cerdo/index.html"), name='cerdo'),
     path('promo/', TemplateView.as_view(template_name="promo/index.html"), name='promo'),
+    path('test/', TemplateView.as_view(template_name="test.html"), name='test'),
 
 ]
